bb2ad9bedd1629daf49aec4e6fdbef7d9b157f9a
Rounak Sahni <rounaksahni2002@gmail.com>
2024-11-11 13:01:55 +0530

    first commit

---
+++ demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Demo  from './PageCom/Demo'
+
+function App() {
+  return (
+    <>
+       <h1>App Component</h1>
+       <Demo/>
+    </>
+  );
+}
+
+export default App;
---
+++ demo/src/PageCom/Demo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../Asstes/Css/Demo.css'
+const Demo = () => {
+  return (
+    <>
+      <h1 style={{color:'red',backgroundColor:''}}>Demo Component</h1>
+    </>
+  )
+}
+
+export default Demo
